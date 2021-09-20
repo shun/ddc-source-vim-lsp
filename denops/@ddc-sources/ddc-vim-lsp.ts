e8a156bd2abc3d0c12500f20d82d04408f4b8296
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2021-09-20 19:30:38 +0900

    update for ddc.vim v0.13.0

--- denops/@ddc-sources/ddc-vim-lsp.ts
+++ denops/@ddc-sources/ddc-vim-lsp.ts
@@ -1,22 +1,25 @@
 import {
   BaseSource,
   Candidate,
-} from "https://deno.land/x/ddc_vim@v0.6.0/types.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.13.0/types.ts#^";
 
 import {
   batch,
   Denops,
   vars,
-} from "https://deno.land/x/ddc_vim@v0.6.0/deps.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.13.0/deps.ts#^";
+
+type Params = {
+};
 
 export class Source extends BaseSource {
   async onInit(args: {
     denops: Denops,
   }): Promise<void> {
     await batch(args.denops, async (denops) => {
-      vars.g.set(denops, "ddc#source#ddc_vim_lsp#_results", []);
-      vars.g.set(denops, "ddc#source#ddc_vim_lsp#_requested", false);
-      vars.g.set(denops, "ddc#source#ddc_vim_lsp#_prev_input", "");
+      await vars.g.set(denops, "ddc#source#ddc_vim_lsp#_results", []);
+      await vars.g.set(denops, "ddc#source#ddc_vim_lsp#_requested", false);
+      await vars.g.set(denops, "ddc#source#ddc_vim_lsp#_prev_input", "");
     });
   }
 
@@ -37,14 +40,18 @@ export class Source extends BaseSource {
     }
 
     await batch(args.denops, async (denops) => {
-      vars.g.set(denops, "ddc#source#ddc_vim_lsp#_results", []);
-      vars.g.set(denops, "ddc#source#ddc_vim_lsp#_requested", false);
-      vars.g.set(denops, "ddc#source#ddc_vim_lsp#_prev_input", args.context.input);
+      await vars.g.set(denops, "ddc#source#ddc_vim_lsp#_results", []);
+      await vars.g.set(denops, "ddc#source#ddc_vim_lsp#_requested", false);
+      await vars.g.set(denops, "ddc#source#ddc_vim_lsp#_prev_input", args.context.input);
 
       // NOTE: choose first lsp server
-      denops.call("ddc_vim_lsp#request", lspservers[0]);
+      await denops.call("ddc_vim_lsp#request", lspservers[0]);
     });
 
     return [];
+  }
+
+  params(): Params {
+    return {};
   }
 }
